126f90afcffe26b83080a1143665bda8abbc1474
miaorz <runze1992@163.com>
2023-04-28 17:28:59 +0900

    静态登录页面 追加

---
+++ src/screens/SignIn/SignInScreen.tsx
@@ -0,0 +1,227 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StatusBar,
+  StyleSheet,
+  Alert,
+} from "react-native";
+
+import * as Animatable from "react-native-animatable";
+import LinearGradient from "react-native-linear-gradient";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Feather from "react-native-vector-icons/Feather";
+
+import { useTheme } from "react-native-paper";
+
+import { AuthContext } from "../../components/context";
+
+import Users from "../../model/users";
+
+import styles from "./styles"
+
+const SignInScreen = () => {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+  });
+
+  const { colors } = useTheme();
+
+  const { signIn } = React.useContext(AuthContext);
+
+  // 检测用户长度
+  const handleUserChange = (val: any) => {
+    if (val.trim().length <= 8) {
+      setData({
+        ...data,
+        username: val,
+        check_textInputChange: true,
+        isValidUser: true,
+      });
+    } else {
+      setData({
+        ...data,
+        username: val,
+        check_textInputChange: false,
+        isValidUser: false,
+      });
+    }
+  };
+
+  // 检测密码长度
+  const handlePasswordChange = (val: any) => {
+    if (val.trim().length <= 16) {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: true,
+      });
+    } else {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: false,
+      });
+    }
+  };
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
+  // 登录按钮
+  const loginHandle = (userName: any, password: any) => {
+    const foundUser = Users.filter((item) => {
+      return userName == item.username && password == item.password;
+    });
+
+    if (data.username.length == 0 || data.password.length == 0) {
+      Alert.alert("错误输入", "用户名或密码不能为空", [{ text: "好的" }]);
+      return;
+    }
+
+    if (foundUser.length == 0) {
+      Alert.alert("登录信息无效", "用户名或密码错误", [{ text: "好的" }]);
+      return;
+    }
+
+    signIn();
+
+    console.log("准备搭建上下文");
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#009387" barStyle="light-content" />
+      <View style={styles.header}>
+        <Text style={styles.text_header}>CloudByte株式会社</Text>
+      </View>
+      <Animatable.View
+        animation="fadeInUpBig"
+        style={[
+          styles.footer,
+          {
+            backgroundColor: colors.background,
+          },
+        ]}
+      >
+        <Text
+          style={[
+            styles.text_footer,
+            {
+              color: colors.shadow,
+            },
+          ]}
+        >
+          用户名
+        </Text>
+        <View style={styles.action}>
+          <FontAwesome name="user-o" color="black" size={20} />
+          <TextInput
+            placeholder="请输入用户名"
+            placeholderTextColor="#666666"
+            style={[
+              styles.textInput,
+              {
+                color: "black",
+              },
+            ]}
+            autoCapitalize="none"
+            onChangeText={(val) => handleUserChange(val)}
+          />
+          {data.check_textInputChange ? (
+            <Animatable.View animation="bounceIn">
+              <Feather name="check-circle" color="#0088CC" size={20} />
+            </Animatable.View>
+          ) : null}
+        </View>
+        {data.isValidUser ? null : (
+          <Animatable.View animation="fadeInLeft" duration={500}>
+            <Text style={styles.errorMsg}>用户名长度不超过8位</Text>
+          </Animatable.View>
+        )}
+
+        <Text
+          style={[
+            styles.text_footer,
+            {
+              color: "black",
+              marginTop: 35,
+            },
+          ]}
+        >
+          密码
+        </Text>
+        <View style={styles.action}>
+          <Feather name="lock" color="black" size={20} />
+          <TextInput
+            placeholder="请输入密码"
+            placeholderTextColor="#666666"
+            secureTextEntry={data.secureTextEntry ? true : false}
+            style={[
+              styles.textInput,
+              {
+                color: "black",
+              },
+            ]}
+            autoCapitalize="none"
+            onChangeText={(val) => handlePasswordChange(val)}
+          />
+          <TouchableOpacity onPress={updateSecureTextEntry}>
+            {data.secureTextEntry ? (
+              <Feather name="eye-off" color="#0088CC" size={20} />
+            ) : (
+              <Feather name="eye" color="grey" size={20} />
+            )}
+          </TouchableOpacity>
+        </View>
+        {data.isValidPassword ? null : (
+          <Animatable.View animation="fadeInLeft" duration={500}>
+            <Text style={styles.errorMsg}>密码长度为8~16</Text>
+          </Animatable.View>
+        )}
+
+        <TouchableOpacity>
+          <Text style={{ color: "#0088CC", marginTop: 15 }}>忘记密码?</Text>
+        </TouchableOpacity>
+
+        <View style={styles.button}>
+          <TouchableOpacity
+            style={styles.signIn}
+            onPress={() => {
+              loginHandle(data.username, data.password);
+            }}
+          >
+            <LinearGradient
+              colors={["#0088CC", "#0088CC"]}
+              style={styles.signIn}
+            >
+              <Text
+                style={[
+                  styles.textSign,
+                  {
+                    color: "#fff",
+                  },
+                ]}
+              >
+                登录
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </View>
+  );
+};
+
+export default SignInScreen;
