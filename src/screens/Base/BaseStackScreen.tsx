64bb7291e1334f85a8a6d4b2c28839e7f86dd403
miaorz <runze1992@163.com>
2023-04-29 11:16:09 +0900

    删除欢迎页面

--- src/screens/Base/BaseStackScreen.tsx
+++ src/screens/Base/BaseStackScreen.tsx
@@ -3,12 +3,10 @@ import React from "react";
 import {createStackNavigator} from '@react-navigation/stack';
 
 import SignInScreen from '../SignIn/SignInScreen';
-import SplashScreen from "../SplashScreen/SplashScreen";
 const RootStack = createStackNavigator();
 
 const RootStackScreen = ({navigation}:any) =>(
     <RootStack.Navigator screenOptions={{ headerShown:false}}>
-        <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
         <RootStack.Screen name="SignInScreen" component={SignInScreen} />
     </RootStack.Navigator>
 )
